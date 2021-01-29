e815312cefdcbd0c611d77f70c0c68ec0a4a580b
Piotr Olearczyk <picu63@gmail.com>
2021-01-29 20:41:25 +0100

    Added static property

---
+++ SeleniumLib/SelDriver.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumLib
+{
+    public class SelDriver
+    {
+        public static void CreateNew(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+        public static IWebDriver WebDriver { get; private set; }
+    }
+}
